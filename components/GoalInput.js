68b94237ef047f0482c957be841829ae2a1b25df
teataeho <rlaxogh9504@gmail.com>
2023-07-17 16:16:50 +0900

    # 4. GoalInput 컴포넌트 분리, 삭제 로직 작성 중...

---
+++ components/GoalInput.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Button, StyleSheet, TextInput, View } from 'react-native'
+
+const GoalInput = (props) => {
+
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  
+  //사용자가 내용을 입력할 때 해당 입력을 가져오는 함수
+  const goalInputHandler = (enteredText) => {
+    // console.log(enteredText);
+    setEnteredGoalText(enteredText);
+  };
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder='할 일을 입력하세요~!'
+        onChangeText={goalInputHandler}
+      />
+      <Button title='할 일 추가하기' onPress={props.addGoalHandler} />
+    </View>
+  )
+}
+
+export default GoalInput
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+});
